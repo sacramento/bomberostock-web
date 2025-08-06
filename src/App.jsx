29f5de9bb1829ca2080dddb376134d7bbddf8849
Nicolás Contreras <setecneuquen@gmail.com>
2025-08-06 11:07:38 -0300

    Estilo: mejorado color de botón Gestionar Usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -236,7 +236,7 @@ function App() {
         <div className="header">
         <div className="logo-container">
           <svg className="logo" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" stroke="currentColor" strokeWidth="2" fill="none" />
+            <path <img src="/bvsma.svg" alt="Logo" className="logo" /> 
           </svg>
           <h1>Materiales BV SMA</h1>
         </div>
